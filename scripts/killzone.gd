e3dbda3cf2d0e2a9ecc584eef59696ae0eb73b59
wafflas <giannikakisgiwrgos@gmail.com>
2026-02-16 00:37:01 +0200

    first commit

---
+++ scripts/killzone.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+@onready var timer: Timer = $Timer
+
+func _on_body_entered(body) -> void:
+	print("you died")
+	timer.start()
+
+func _on_timer_timeout() -> void:
+	get_tree().reload_current_scene()
